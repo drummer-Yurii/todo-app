78a2889af04a2799f9e10c52505f2d3228a45a22
Yurii Rybachok <drummer2009@ukr.net>
2023-11-12 15:44:38 +0200

    section with title & name. & save in localStorage

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,22 @@ const input_category = ref(null);
 const todos_asc = computed(() => todos.value.sort((a, b) => {
   return b.createdAt - a.createdAt
 }))
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
-  
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
