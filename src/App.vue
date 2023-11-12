262b3f70ba2ab606017b49764b4686fe43bd7a77
Yurii Rybachok <drummer2009@ukr.net>
2023-11-12 15:36:50 +0200

    add data & functionality for todo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { ref, onMounted, computed, watch } from 'vue';
 
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
 </script>
 
 <template>
-  <div>
-    Hello, World!
-  </div>
+  
 </template>
